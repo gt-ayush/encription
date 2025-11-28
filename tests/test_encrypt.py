3bbbe7c3f3b6018ec7addf67ed38ad72ce41631d
gt-ayush <gt-ayush@outlook.com>
2025-11-28 22:43:54 +0530

    test_encript

---
+++ tests/test_encrypt.py
@@ -0,0 +1,15 @@
+from keygen import generate_key
+from encryptor import xor_encrypt, xor_decrypt
+
+def run():
+    text = input("Enter text to encrypt: ")
+
+    key = generate_key()
+    encrypted = xor_encrypt(text, key)
+    decrypted = xor_decrypt(encrypted, key)
+
+    print("\n🔐 Encrypted:", encrypted)
+    print("🔓 Decrypted:", decrypted)
+
+if __name__ == "__main__":
+    run()
